76419861928e76c32512aea4bf658bea5e6a5dd6
Daniel Bucket <danieljbucket@gmail.com>
2025-03-19 20:15:38 -0600

    allow all incoming urls

--- app/src/v1/app.js
+++ app/src/v1/app.js
@@ -11,13 +11,14 @@ const { DEV_URL, NODE_ENV } = process.env;
 const whitelist = ['http://localhost:5173', 'https://bucketlab.io', 'https://www.bucketlab.io'];
 
 const corsOptions = {
-  origin: (origin, cb) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      cb(null, true);
-    } else {
-      cb(new Error(`That domain is not CORS whitelisted. Origin: ${origin}`));
-    };
-  },
+    origin: '*', // Allow all origins by default
+  // origin: (origin, cb) => {
+  //   if (whitelist.indexOf(origin) !== -1) {
+  //     cb(null, true);
+  //   } else {
+  //     cb(new Error(`That domain is not CORS whitelisted. Origin: ${origin}`));
+  //   };
+  // },
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   optionsSuccessStatus: 200
