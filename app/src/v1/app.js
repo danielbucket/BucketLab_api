9df757520769056e2f663c3ea7ef5b568f311132
Daniel Bucket <danieljbucket@gmail.com>
2025-03-21 16:06:31 -0600

    trying to solve cors barriers

--- app/src/v1/app.js
+++ app/src/v1/app.js
@@ -11,13 +11,8 @@ const { DEV_URL, NODE_ENV } = process.env;
 const whitelist = ['http://localhost:5173', 'https://bucketlab.io', 'https://www.bucketlab.io'];
 
 const corsOptions = {
-  origin: (origin, cb) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      cb(null, true);
-    } else {
-      cb(new Error(`That domain is not CORS whitelisted. Origin: ${origin}`));
-    };
-  },
+  origin: whitelist,
+  credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   optionsSuccessStatus: 200
