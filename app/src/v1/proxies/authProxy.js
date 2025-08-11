56323133a0ef4fa0e48ffb7be9f299cf219eee8e
Daniel Bucket <danieljbucket@gmail.com>
2025-08-10 21:16:09 -0600

    ...

--- app/src/v1/proxies/authProxy.js
+++ app/src/v1/proxies/authProxy.js
@@ -1,22 +1,17 @@
 const {
   createProxyMiddleware,
-  debugProxyErrorsPlugin, // subscribe to proxy errors to prevent server from crashing
   loggerPlugin, // log proxy events to a logger (ie. console)
   errorResponsePlugin, // return 5xx response on proxy error
-  proxyEventsPlugin, // implements the "on:" option
-  fixRequestBody, // fix request body for POST/PUT requests
 } = require('http-proxy-middleware');
 
 exports.authProxy = () => createProxyMiddleware({
   target: 'http://auth_server:4021',
   changeOrigin: true,
-  // onProxyReq: fixRequestBody, // Fix request body for POST/PUT requests
   pathRewrite: {
     '^/auth': '', // Remove /auth prefix when forwarding to auth server
   },
   onProxyReq: (proxyReq, req, res) => {
     console.log(`Proxying request to Auth_Server from App_Server: ${req.method} ${req.originalUrl}`);
-    // Modify proxy request headers or body if needed
     if (req.body) {
       const bodyData = JSON.stringify(req.body);
       proxyReq.setHeader('Content-Type', 'application/json');
@@ -26,10 +21,8 @@ exports.authProxy = () => createProxyMiddleware({
   },
   onProxyRes: (proxyRes, req, res) => {
     console.log(`Received response from Auth_Server: ${proxyRes.statusCode}`);
-    // Modify response headers if needed
     res.setHeader('X-Proxy-Response', 'Auth_Server');
   },
-  logger: console, // Use console for logging proxy events
   onError: (err, req, res) => {
     console.error('Proxy error:', err);
     res.status(500).json({
@@ -37,5 +30,5 @@ exports.authProxy = () => createProxyMiddleware({
       message: 'Internal server error while proxying to Laboratory Server.'
     });
   },
-  plugins: [debugProxyErrorsPlugin, loggerPlugin, errorResponsePlugin, proxyEventsPlugin],
+  plugins: [loggerPlugin, errorResponsePlugin],
 });
