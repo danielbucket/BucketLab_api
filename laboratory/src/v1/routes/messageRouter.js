6d279aefdc03409dcd3126277b4993825fb3940c
Daniel Bucket <danieljbucket@gmail.com>
2025-08-13 13:19:27 -0600

    ..

--- laboratory/src/v1/routes/messageRouter.js
+++ laboratory/src/v1/routes/messageRouter.js
@@ -1,24 +1,23 @@
 const router = require('express').Router();
-const cors = require('cors');
 const { POST_, PATCH_, DELETE_, GET_ } = require('../controllers/MessagesController');
 
 router.route('/')
-  .get(cors(), GET_.get_all_messages)
-  .post(cors(), POST_.new_message);
+  .get(GET_.get_all_messages)
+  .post(POST_.new_message);
 
 router.route('/sender/:id')
-  .get(cors(), GET_.get_messages_by_sender_id);
+  .get(GET_.get_messages_by_sender_id);
 
 router.route('/receiver/:id')
-  .get(cors(), GET_.get_messages_by_receiver_id);
+  .get(GET_.get_messages_by_receiver_id);
 
 router.route('/message/:id')
-  .get(cors(), GET_.get_message_by_message_id);
+  .get(GET_.get_message_by_message_id);
 
 router.route('/delete/:id')
-  .delete(cors(), DELETE_.delete_message_by_message_id);
+  .delete(DELETE_.delete_message_by_message_id);
 
 router.route('/update/:id')
-  .patch(cors(), PATCH_.update_message_by_message_id);
+  .patch(PATCH_.update_message_by_message_id);
 
 module.exports = router;
