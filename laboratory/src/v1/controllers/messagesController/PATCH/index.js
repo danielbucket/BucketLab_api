e4e66952112a4d7327adb62038b9450cbe196c04
Daniel Bucket <danieljbucket@gmail.com>
2025-08-12 19:57:33 -0600

    ...

--- laboratory/src/v1/controllers/messagesController/PATCH/index.js
+++ laboratory/src/v1/controllers/messagesController/PATCH/index.js
@@ -1,5 +1,7 @@
 const { update_message_by_message_id } = require('./update_message_by_message_id');
 
-module.exports = {
-  update_message_by_message_id
-};
+module.exports = Object.freeze(
+  Object.assign({},
+    { update_message_by_message_id }
+  )
+);
