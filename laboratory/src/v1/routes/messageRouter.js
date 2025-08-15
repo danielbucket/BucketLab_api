ff0ee85f9bdc9780351b9f6f44b496d5a8ba6370
Daniel Bucket <danieljbucket@gmail.com>
2025-08-14 21:31:33 -0600

    messagesRouter rebuild stage one

--- laboratory/src/v1/routes/messageRouter.js
+++ laboratory/src/v1/routes/messageRouter.js
@@ -1,44 +1,24 @@
 const router = require('express').Router();
 const cors = require('cors');
+const MessagesController = require('../controllers/MessagesController');
 
 router.route('/')
-  .get(cors(), (req, res) => {
-    // Controller logic for getting all messages
-    res.json({ message: 'Get all messages' });
-  })
-  .post(cors(), (req, res) => {
-    // Controller logic for creating a new message
-    res.json({ message: 'Create a new message' });
-  });
+  .get(cors(), MessagesController.getAllMessages)
+//   .post(cors(), MessagesController.createMessage);
 
-router.route('/sender/:id')
-  .get(cors(), (req, res) => {
-    // Controller logic for getting messages by sender ID
-    res.json({ message: `Get messages by sender ID: ${req.params.id}` });
-  });
+// router.route('/sender/:id')
+//   .get(cors(), MessagesController.getMessagesBySenderId);
 
-router.route('/receiver/:id')
-  .get(cors(), (req, res) => {
-    // Controller logic for getting messages by receiver ID
-    res.json({ message: `Get messages by receiver ID: ${req.params.id}` });
-  });
+// router.route('/receiver/:id')
+//   .get(cors(), MessagesController.getMessagesByReceiverId);
 
-router.route('/message/:id')
-  .get(cors(), (req, res) => {
-    // Controller logic for getting a message by message ID
-    res.json({ message: `Get message by message ID: ${req.params.id}` });
-  });
+// router.route('/message/:id')
+//   .get(cors(), MessagesController.getMessageById);
 
-router.route('/delete/:id')
-  .delete(cors(), (req, res) => {
-    // Controller logic for deleting a message by message ID
-    res.json({ message: `Delete message by message ID: ${req.params.id}` });
-  });
+// router.route('/delete/:id')
+//   .delete(cors(), MessagesController.deleteMessageById);
 
-router.route('/update/:id')
-  .patch(cors(), (req, res) => {
-    // Controller logic for updating a message by message ID
-    res.json({ message: `Update message by message ID: ${req.params.id}` });
-  });
+// router.route('/update/:id')
+//   .patch(cors(), MessagesController.updateMessageById);
 
 module.exports = router;
