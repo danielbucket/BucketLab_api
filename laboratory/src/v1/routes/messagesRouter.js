984f7ba7b54b4ca9d6908dd74a0318048079b485
Daniel Bucket <danieljbucket@gmail.com>
2025-08-15 19:40:43 -0600

    Fix UsersController function references to use camelCase naming convention

--- laboratory/src/v1/routes/messagesRouter.js
+++ laboratory/src/v1/routes/messagesRouter.js
@@ -1,26 +1,26 @@
 const cors = require('cors');
 
-const { DELETE_, GET_, PATCH_, POST_ } = require('../controllers/MessagesController'); 
+const { DELETE, GET, PATCH, POST } = require('../controllers/MessagesController'); 
 
 const messagesRouter = require('express').Router();
 
 messagesRouter.route('/')
-  .get(cors(), GET_.get_all_messages)
-  .post(cors(), POST_.new_message);
+  .get(cors(), GET.get_all_messages)
+  .post(cors(), POST.new_message);
 
 messagesRouter.route('/sender/:id')
-  .get(cors(), GET_.get_messages_by_sender_id);
+  .get(cors(), GET.get_messages_by_sender_id);
 
 messagesRouter.route('/receiver/:id')
-  .get(cors(), GET_.get_messages_by_receiver_id);
+  .get(cors(), GET.get_messages_by_receiver_id);
 
 messagesRouter.route('/message/:id')
-  .get(cors(), GET_.get_message_by_message_id);
+  .get(cors(), GET.get_message_by_message_id);
 
 messagesRouter.route('/delete/:id')
-  .delete(cors(), DELETE_.delete_message_by_message_id);
+  .delete(cors(), DELETE.delete_message_by_message_id);
 
 messagesRouter.route('/update/:id')
-  .patch(cors(), PATCH_.update_message_by_message_id);
+  .patch(cors(), PATCH.update_message_by_message_id);
 
 module.exports = messagesRouter;
