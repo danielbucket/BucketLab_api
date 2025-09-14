0129779f6ff81f56bfdd867ba1fa00a824df25df
Daniel Bucket <danieljbucket@gmail.com>
2025-09-14 15:04:35 -0600

    update accounts_server

--- accounts/src/v1/app.js
+++ accounts/src/v1/app.js
@@ -9,20 +9,13 @@ app.set('trust proxy', true);
 app.use(cors(corsConfig()));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'BucketLab Empire Accounts API root endpoint.',
-  });
-});
-
 app.use('/', (req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(`Request received at Accounts_Server @ ${req.requestTime} for ${req.originalUrl}`);
   next();
 });
 
-app.use('/accnt', accountsRouter);
+app.use('/', accountsRouter);
 
 app.all('/*', (req, res) => {
   res.status(404).json({
