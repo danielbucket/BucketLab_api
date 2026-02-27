eeff6aceb1fc77120a04c5c9022bad7e29333aaa
Daniel Bucket <danieljbucket@gmail.com>
2026-02-27 15:09:21 -0700

    modify routing names

--- laboratory/src/v1/controllers/messagesController/index.js
+++ laboratory/src/v1/controllers/messagesController/index.js
@@ -1,9 +1,7 @@
 
-module.exports = Object.freeze(
-  Object.assign({},
-    { DELETE_methods: require('./DELETE') },
-    { GET_methods: require('./GET') },
-    { PATCH_methods: require('./PATCH') },
-    { POST_methods: require('./POST') }
-  )
-);
+module.exports = {
+  DELETE_methods: require('./DELETE'),
+  GET_methods: require('./GET'),
+  PATCH_methods: require('./PATCH'),
+  POST_methods: require('./POST')
+};
