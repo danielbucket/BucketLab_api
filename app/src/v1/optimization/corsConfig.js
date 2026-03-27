de81f0108fdbd031cddc54756a65339439740fbf
Daniel Bucket <danieljbucket@gmail.com>
2026-03-27 15:42:59 -0600

    so dumb.. I accidentally replaced the tunnel token with the dev tunnel.

--- app/src/v1/optimization/corsConfig.js
+++ app/src/v1/optimization/corsConfig.js
@@ -3,13 +3,13 @@ const { NODE_ENV } = process.env;
 const whitelist = ['https://bucketlab.io', 'http://localhost:5173'];
 
 exports.corsConfig = () => {
+
   return {
     preflightContinue: false,
     optionsSuccessStatus: 204,
     maxAge: 86400, // 24 hours in seconds
     exposedHeaders: ['Content-Length', 'X-Response-Time'],
     origin: (origin, callback) => {
-      console.log(`CORS check for origin: ${origin} at ${new Date().toISOString()}`);
       if (NODE_ENV === 'development') {
         return callback(null, true); // Allow all origins in development
       }
